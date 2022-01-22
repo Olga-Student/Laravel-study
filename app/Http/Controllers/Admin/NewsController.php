6f3a675534009055f9ff0f26506bf03f8130d726
Olga-Student <orazina.2906@mail.ru>
2022-01-22 16:52:51 +0300

    l

---
+++ app/Http/Controllers/Admin/NewsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class NewsController extends Controller
+{
+    public function index()
+    {
+        dd('TEST');
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+}
